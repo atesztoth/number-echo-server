56cea052bbe45725efdc70048a20f79a7534448b
Attila TÃth <muchcoffe@gmail.com>
2018-08-25 17:07:56 +0200

    dotenv added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const debug = require('debug')('api:Server');
+const debug = require('debug')('app:Server');
 const app = require('./app');
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
-
 debug('Application is listening on port: ' + PORT);
+app.listen(PORT);
